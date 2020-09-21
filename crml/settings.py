55505677f2dfbecf894260961022a6c772c01587
Herasymov <Slavka.gs98@gmail.com>
2020-09-21 17:18:57 +0300

    Connect to PostgreSql

--- crml/settings.py
+++ crml/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'crml.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cml_db',
+        'USER': 'myusername',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
