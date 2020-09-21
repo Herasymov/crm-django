f392027fdf741c1f8b7e93dde1d42fa1ffe3622f
Herasymov <Slavka.gs98@gmail.com>
2020-09-21 14:33:19 +0300

    New changes

--- accounts/views.py
+++ accounts/views.py
@@ -9,56 +9,53 @@ from django.contrib.auth.models import Group
 from .models import *
 from .forms import *
 from .filters import *
+from .decorators import *
 
 
+@unautheenticated_user
 def registerPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+            Customer.objects.create(
+                user=user,
+                )
+            messages.success(request, 'Account was created for ' + username)
+            return redirect('login')
+    context = {'form':form}
+    return render(request, 'accounts/register.html', context)
 
-	form = CreateUserForm()
-	if request.method == 'POST':
-		form = CreateUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			username = form.cleaned_data.get('username')
-
-
-			messages.success(request, 'Account was created for ' + username)
-
-			return redirect('login')
-
-
-	context = {'form':form}
-	return render(request, 'accounts/register.html', context)
-
-
+@unautheenticated_user
 def loginPage(request):
-
-	if request.method == 'POST':
-		username = request.POST.get('username')
-		password =request.POST.get('password')
-
-		user = authenticate(request, username=username, password=password)
-
-		if user is not None:
-			login(request, user)
-			return redirect('home')
-		else:
-			messages.info(request, 'Username OR password is incorrect')
-
-	context = {}
-	return render(request, 'accounts/login.html', context)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 def logoutUser(request):
-	logout(request)
-	return redirect('login')
+    logout(request)
+    return redirect('login')
 
 @login_required(login_url='login')
+@admin_only
 def home(request):
     customers = Customer.objects.all()
     orders = Order.objects.all()
 
     total_customers = customers.count()
     total_orders = orders.count()
-
     delivered = orders.filter(status = 'Delivered').count()
     pending = orders.filter(status = 'Pending').count()
 
@@ -67,10 +64,39 @@ def home(request):
     'delivered':delivered, 'pending':pending}
     return render(request, 'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
+@allowed_user(allowed_roles=['customer'])
+def userPage(request):
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status = 'Delivered').count()
+    pending = orders.filter(status = 'Pending').count()
+    context = {'orders':orders, 'total_orders':total_orders,
+    'delivered':delivered, 'pending':pending}
+    return render(request, 'accounts/user.html', context)
+
+@login_required(login_url='login')
+@allowed_user(allowed_roles=['customer'])
+def accountSettings(request):
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request, 'accounts/account_settings.html', context)
+
+@login_required(login_url='login')
+@allowed_user(allowed_roles=['admin'])
 def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products':products})
 
+@login_required(login_url='login')
+@allowed_user(allowed_roles=['admin'])
 def customer(request, pk_test):
     customer = Customer.objects.get(id=pk_test)
 
@@ -84,6 +110,8 @@ def customer(request, pk_test):
     'myFilter':myFilter}
     return render(request, 'accounts/customer.html',context)
 
+@login_required(login_url='login')
+@allowed_user(allowed_roles=['admin'])
 def createOrder(request, pk):
     OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra =5)
     customer = Customer.objects.get(id=pk)
@@ -97,6 +125,8 @@ def createOrder(request, pk):
 
     return render(request, 'accounts/order_form.html', context)
 
+@login_required(login_url='login')
+@allowed_user(allowed_roles=['admin'])
 def updateOrder(request, pk):
 
     order = Order.objects.get(id=pk)
@@ -111,6 +141,8 @@ def updateOrder(request, pk):
 
     return render(request, 'accounts/order_form.html', context)
 
+@login_required(login_url='login')
+@allowed_user(allowed_roles=['admin'])
 def deleteOrder(request, pkd):
 
     order = Order.objects.get(id=pkd)
--- crml/urls.py
+++ crml/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
-
 from django.http import HttpResponse
-
+from django.conf.urls.static import static
+from django.conf import settings
 def home(request):
     return HttpResponse('Home page')
 
@@ -31,3 +31,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', home),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
